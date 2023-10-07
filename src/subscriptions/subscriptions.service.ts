3cf7c3875e7d33d2524a76c621b5e9039c293475
abenikeb <abenikeb79@gmail.com>
2023-10-07 08:56:08 +0300

    INitaialization fixed

--- src/subscriptions/subscriptions.service.ts
+++ src/subscriptions/subscriptions.service.ts
@@ -1,7 +1,3 @@
-// import { Injectable } from '@nestjs/common';
-// import { CreateSubscriptionDto } from './dto/create-subscription.dto';
-// import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
-
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateSubscriptionDto } from './dto/create-subscription.dto';
 import { InjectRepository } from '@nestjs/typeorm';
